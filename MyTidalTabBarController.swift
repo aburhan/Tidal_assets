57717d1a90ab2120bbdd1b8981714d57a02f68bc
Ameenah Burhan <ameenah.burhan@gmail.com>
2017-08-06 16:39:26 -0400

    Create MyTidalTabBarController.swift
    tab bar controller

---
+++ MyTidalTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MyTidalTabBarController.swift
+//  Tidal2
+//
+//  Created by Ameenah Burhan on 7/31/17.
+//  Copyright © 2017 Ameenah Burhan. All rights reserved.
+//
+
+import UIKit
+
+class MyTidalTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().barTintColor = UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 1)
+    }
+
+}
